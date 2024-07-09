c5e62f872f395a4ca6d7d6406cc1c3e125614209
Albert Gil López <albert@albertgilopez.es>
2024-07-09 17:13:10 +0200

    Mover basic-concepts, hello-rust, threads y .gitignore a la raíz del proyecto

---
+++ GameOfLife/target/debug/GameofLife.d
@@ -0,0 +1,1 @@
+C:\Users\Usuario\Downloads\RUST\GameOfLife\target\debug\GameofLife.exe: C:\Users\Usuario\Downloads\RUST\GameOfLife\src\board.rs C:\Users\Usuario\Downloads\RUST\GameOfLife\src\main.rs
---
+++ GameOfLife/target/debug/deps/GameofLife.d
@@ -0,0 +1,6 @@
+C:\Users\Usuario\Downloads\RUST\GameOfLife\target\debug\deps\GameofLife.exe: src/main.rs src\board.rs
+
+C:\Users\Usuario\Downloads\RUST\GameOfLife\target\debug\deps\GameofLife.d: src/main.rs src\board.rs
+
+src/main.rs:
+src\board.rs:
